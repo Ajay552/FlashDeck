5032a38548ffa7c7e8804164cbbbeb019d7ca953
Ajay S <72488450+Ajay552@users.noreply.github.com>
2022-11-26 17:02:20 +0000

    setup mongodb and added gitignore for node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import DeckModel from './models/Deck';
+
+const PORT = 5000;
+
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("hello World");
-})
+// const db = mongoose.connect('mongodb+srv://nova:OnePiece@cluster0.cpxrfgs.mongodb.net/?retryWrites=true&w=majority');
 
-app.listen(5000);
+app.get('/', (req: Request, res: Response) => {
+    res.send("hello World!");
+});
+
+mongoose.connect(
+    'mongodb+srv://nova:OnePiece@cluster0.cpxrfgs.mongodb.net/?retryWrites=true&w=majority'
+    ).then(() => {
+        console.log(`listening to port ${PORT}`);
+        app.listen(PORT);
+    })
