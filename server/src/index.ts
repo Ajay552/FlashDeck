75b216fbc35de4705e9bea1da47012314bd0fbbe
Ajay S <72488450+Ajay552@users.noreply.github.com>
2022-11-27 07:36:58 +0000

    created a .env file with mongo db url to secure the password

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,30 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import { config } from 'dotenv';
+
 import Deck from './models/Deck';
 
+config();
 const PORT = 5000;
 
 const app = express();
 
-// const db = mongoose.connect('mongodb+srv://nova:OnePiece@cluster0.cpxrfgs.mongodb.net/?retryWrites=true&w=majority');
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("hello World!");
+    res.send("hello World! Test");
 });
 
 app.post("/decks", async (req: Request, res: Response) => {
+    const title = req.body.title;
     const newDeck = new Deck({
-        title: "Flash card deck",
+        title: title,
     });
     const createdDeck = await newDeck.save();
     res.json(createdDeck);
 })
 
-mongoose.connect(
-    'mongodb+srv://nova:OnePiece@cluster0.cpxrfgs.mongodb.net/?retryWrites=true&w=majority'
-).then(() => {
+mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`listening to port ${PORT}`);
     app.listen(PORT);
 })
